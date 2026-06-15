fd0cdfd43b4d4cc5ef0d299f81d0e07035706ceb
Komokata <2026029@fsc-educ.com>
2026-06-15 17:21:22 +0900

    あと一つ

--- init.sql
+++ init.sql
@@ -10,3 +10,45 @@ INSERT INTO questions (title, body) VALUES
   ('Docker Compose で MySQL を起動したい', 'compose.yaml の書き方を教えてください。');
 
   CREATE DATABASE dotachan;
+  CREATE TABLE User(
+    id VARCHAR(50) NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    reviewed BOOLEAN NOT NULL
+  );
+    CREATE TABLE Store(
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(100) NOT NULL,
+   email VARCHAR(100) NOT NULL,
+   password VARCHAR(30) NOT NULL,
+   address VARCHAR(100)NOT NULL,
+   phone_range INT NOT NULL,
+   payment VARCHAR(100) NOT NULL,
+   price_range INT,
+   genre VARCHAR(100) NOT NULL,
+   coupon VARCHAR(1000),
+   free_desc VARCHAR(1000)
+  );
+  CREATE TABLE Sale(
+    store_id INT           NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(100)  NOT NULL,
+    contents VARCHAR(1000) NOT NULL,
+    FOREIGN KEY (store_id)   REFERENCES Store(id)
+  );
+   CREATE TABLE Suddensale(
+    store_id INT AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    contents VARCHAR(1000) NOT NULL,
+    completed BOOLEAN NOT NULL,
+    FOREIGN KEY (store_id)  REFERENCES Store(id)
+  );
+
+
+  CREATE TABLE StoreReview(
+    store_id INT AUTO_INCREMENT,
+    comments VARCHAR(1000) NOT NULL,
+    evaluation BOOLEAN NOT NULL,
+    user_id VARCHAR(50) NOT NULL,
+    FOREIGN KEY (store_id)  REFERENCES Store(id),
+    FOREIGN KEY (user_id)  REFERENCES User(id)
+  );

ed15c16b3232cb2eb0d6b016526aa3400fd2f427
Iwamoto <=2026018@fsc-educ.com>
2026-06-18 17:30:47 +0900

    多分できた！

--- src/main/java/com/example/macaron/UserSubmitService.java
+++ src/main/java/com/example/macaron/UserSubmitService.java
@@ -15,25 +15,9 @@ public class UserSubmitService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    // mypageに新規ユーザーの情報を追加する
-    // public void userRegister(UserForm form) {
-    // userSubmitRepository.mypage(new User(null, form.getMail(), form.getName(),
-    // form.getPassword(), false));
-    // }
-
     public Optional<User> findById(Long id) {
         return userSubmitRepository.findById(id);
     }
-
-    // public boolean authenticate(String mail, String password) {
-    // Optional<String> hash = userSubmitRepository.findPasswordHash(mail);
-    // //値があればStringで帰ってくるからOptional<String>が戻り値の型
-    // if (hash.isEmpty()) {//DBに値があるかどうか
-    // return false;
-    // }
-    // return passwordEncoder.matches(password, hash.get());
-    // //optionalに含まれるget()メソッド DBから値をとってくる
-    // }
 
     // パスワードをハッシュ化して登録
     public void register(String name, String mail, String password) {

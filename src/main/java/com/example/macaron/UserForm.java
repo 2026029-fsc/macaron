39f19b6a05e585f7a313c4629d475279c1ea9a25
Iwamoto <=2026018@fsc-educ.com>
2026-06-19 17:28:32 +0900

    バリデーションできた！

--- src/main/java/com/example/macaron/UserForm.java
+++ src/main/java/com/example/macaron/UserForm.java
@@ -1,30 +1,46 @@
 package com.example.macaron;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;//メールアドレスの形式をチェック
+import jakarta.validation.constraints.NotBlank;//入力されているかチェック
 import jakarta.validation.constraints.Size;//文字列の長さを制限
 
 //編集するためにfinalなし
 public class UserForm {
+    //ユーザーネーム
+    @NotBlank(message = "ユーザーネームは必須項目です")
+    @Size(max=30,message = "ユーザーネームは30文字以内で入力してください")//ユーザーネームは30文字まで
     private String name;
+       
+    //メールアドレス
+    @NotBlank(message = "メールアドレスは必須項目です")
+    @Size(max = 100, message = "メールアドレスは100文字以内で入力してください")//メールアドレス100文字まで
     @Email(message = "メールアドレスの形式が正しくありません")
-    @Size(max = 100, message = "メールアドレスは100文字以内で入力してください")
     private String mail;
+    
+    //パスワード
+    @NotBlank(message = "パスワードは必須項目です")
+    @Size(min = 8, message = "パスワードは8文字以上で入力してください")//パスワードは最低8文字
     private String password;
-    private String password2;//確認用のパスワード　追加
+
+    //確認用パスワード
+    @NotBlank(message = "パスワードは必須項目です")
+    private String password2;
+
+    //パスワードとパスワード確認用を同じにする
+    @AssertTrue(message = "パスワードとパスワードの確認用は同一にしてください。")
+    public boolean isPasswordValid() {
+		if (password == null || password.isEmpty()) {
+			return true;
+		}
+		
+		return password.equals(password2);
+	} 
+
     private boolean reviewed;
 
-    // public UserForm(int id, String mail, String name, String password, boolean
-    // reviewd) {
-    // this.id = id;
-    // this.mail = mail;
-    // this.name = name;
-    // this.password = password;
-    // this.reviewd = reviewd;
-    // }
-
-    // public int getId() {
-    // return id;
-    // }
+    
+    //ゲッター
     public String getMail() {
         return mail;
     }
@@ -36,13 +52,16 @@ public class UserForm {
     public String getPassword() {
         return password;
     }
+
     public String getPassword2() {
         return password2;
-    }//追加
+    }
+
     public boolean getReviewed() {
         return reviewed;
     }
 
+    //セッター
     public void setMail(String mail) {
         this.mail = mail;
     }
@@ -58,6 +77,7 @@ public class UserForm {
     public void setPassword2(String password2) {
         this.password2 = password2;
     }
+
     public void setReviewd(boolean reviewed) {
         this.reviewed = reviewed;
     }
--- src/main/java/com/example/macaron/UserSubmitController.java
+++ src/main/java/com/example/macaron/UserSubmitController.java
@@ -1,19 +1,19 @@
 package com.example.macaron;
 
-import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserSubmitController {
     private final UserSubmitService userSubmitService;
+    UserForm user = new UserForm();
 
     public UserSubmitController(UserSubmitService userSubmitService) {
         this.userSubmitService = userSubmitService;
@@ -21,16 +21,22 @@ public class UserSubmitController {
 
     // localhost:8080/resisterをブラウザ(URL)で入力したときに表示させる
     @GetMapping("/register")
-    public String register() {
-        // model.addAttribute("form",user());//よくわからない？？
+    public String register(Model model) {
+        model.addAttribute("userform", new UserForm());
         return "dotachan/register";
     }
 
-    // localhost:8080/resisterをブラウザで入力。
-    @PostMapping("/correct")
-    public String userRegister(@ModelAttribute UserForm form) { // 入力した値をModelにわたす。
-        userSubmitService.register(form.getName(), form.getMail(), form.getPassword()); // サービス、に渡す(その後リポジトリを通り、データベースに保存する。)
-        return "redirect:/correct"; // 戻るURLのhtml
+    @PostMapping("/register")
+    // 新規会員登録するときのメソッド
+    // 入力内容でエラーになるとregisterに戻す
+    public String register(@Validated @ModelAttribute("userform") UserForm form, BindingResult result, Model model) {
+        // model.addAttribute("UserForm", user);
+        if (result.hasErrors()) {
+            return "dotachan/register";
+        }
+    // エラーでなければcorrectに行き、入力した値をサービスに渡す↓
+        userSubmitService.register(form.getName(), form.getMail(), form.getPassword());
+        return "redirect:/correct";
     }
 
     // localhost:8080/correctをブラウザで入力
@@ -45,31 +51,4 @@ public class UserSubmitController {
         return "dotachan/subscribe";// 表示させるhtml
     }
 
-    // マイページにデータベースから持ってきた値を表示させたい
-    //これはセッションが必要らしいからこれは違う！
-    // @GetMapping("/mypage/{id}")
-    // public String mypage(@PathVariable Long id, Model model) {// ブラウザでもらった値をjavaに変換、モデルに渡す
-    //     Optional<User> userOpt = userSubmitService.findById(id);// サービスにお願いする(リポジトリを通り、データベースから値をもらう)
-    //     model.addAttribute("user", userOpt.get());// "user"という箱に詰める
-    //     return "mypage/{id}";// 表示させるhtml
-    // }
-
-    @PostMapping("/register")
-    public String register(//登録するときのメソッド
-            @RequestParam String name, @RequestParam String mail, @RequestParam String password,
-            @RequestParam String password2) {
-        
-        userSubmitService.register(name, mail, password);
-        return "redirect:/correct";
-
-        // パスワードと確認用のパスワードが一致しているか
-        // わからん。。。
-        // if (!password.equals(password2)) {
-        //     return "register";
-        // }
-        // return "redirect:/correct";
-
-      
-   
-}
 }

3643f28534adc902590bf4e4496933b5aaac8eff
Nishiyama <=2026056@fsc-educ.com>
2026-06-22 11:22:09 +0900

    特に変更点はなし

--- src/main/java/com/example/macaron/SuddenSaleRepository.java
+++ src/main/java/com/example/macaron/SuddenSaleRepository.java
@@ -14,12 +14,12 @@ public class SuddenSaleRepository {
         this.jdbcClient = jdbcClient;
     }
 
-    // //SuddenSale情報の表示
-    // public List<SuddenSaleview> previewSuddenSale() {
-    //     return jdbcClient.sql("SELECT Suddensale.id, Suddensale.name AS suddensaleName, Store.name AS storeName FROM Suddensale JOIN Store ON Suddensale.store_id = Store.id LIMIT 3")
-    //             .query(SuddenSaleview.class)
-    //             .list();
-    // }
+    //SuddenSale情報の表示
+    public List<SuddenSaleview> previewSuddenSale() {
+        return jdbcClient.sql("SELECT Suddensale.id, Suddensale.name AS suddensaleName, Store.name AS storeName FROM Suddensale JOIN Store ON Suddensale.store_id = Store.id LIMIT 3")
+                .query(SuddenSaleview.class)
+                .list();
+    }
 
     // idで1店舗のみに絞り込んで SuddenSale情報の表示(Storeテーブルとの結合なし)
     public List<SuddenSale> previewSuddenSaleForId(Long id) {
@@ -30,19 +30,19 @@ public class SuddenSaleRepository {
     }
 
 
-    // //SuddenSale情報をジャンル検索
-    // public List<SuddenSaleview> serchByGenre(String keyword) {
-    //     return jdbcClient.sql("SELECT Suddensale.id, Suddensale.name AS suddensaleName, Store.name AS storeName FROM Suddensale JOIN Store ON Suddensale.store_id = Store.id WHERE genre LIKE :keyword LIMIT 3")
-    //             .param("keyword", "%" + keyword + "%")
-    //             .query(SuddenSaleview.class)
-    //             .list();
-    // }
+    //SuddenSale情報をジャンル検索
+    public List<SuddenSaleview> serchByGenre(String keyword) {
+        return jdbcClient.sql("SELECT Suddensale.id, Suddensale.name AS suddensaleName, Store.name AS storeName FROM Suddensale JOIN Store ON Suddensale.store_id = Store.id WHERE genre LIKE :keyword LIMIT 3")
+                .param("keyword", "%" + keyword + "%")
+                .query(SuddenSaleview.class)
+                .list();
+    }
 
-    // //SuddenSale情報をキーワード検索
-    // public List<SuddenSaleview> serchByKeyword(String keyword) {
-    //     return jdbcClient.sql("SELECT Suddensale.id, Suddensale.name AS suddensaleName, Store.name AS storeName FROM Suddensale JOIN Store ON Suddensale.store_id = Store.id WHERE genre LIKE :keyword LIMIT 3")
-    //             .param("keyword", "%" + keyword + "%")
-    //             .query(SuddenSaleview.class)
-    //             .list();
-    // }
+    //SuddenSale情報をキーワード検索
+    public List<SuddenSaleview> serchByKeyword(String keyword) {
+        return jdbcClient.sql("SELECT Suddensale.id, Suddensale.name AS suddensaleName, Store.name AS storeName FROM Suddensale JOIN Store ON Suddensale.store_id = Store.id WHERE genre LIKE :keyword LIMIT 3")
+                .param("keyword", "%" + keyword + "%")
+                .query(SuddenSaleview.class)
+                .list();
+    }
 }

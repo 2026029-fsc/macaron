fddd5ad5b2b24ea74d19ae3d2ea074f0b2121e5e
Nishiyama <=2026056@fsc-educ.com>
2026-06-22 15:15:33 +0900

    セッションを確認

--- src/main/java/com/example/macaron/User.java
+++ src/main/java/com/example/macaron/User.java
@@ -6,16 +6,16 @@ public class User {
     private final String mail;
     private final String name;
     private final String password;
-    private final String password2;
+    // private final String password2;
     private final boolean reviewed;
 
-    public User(Long id, String mail, String name, String password, String password2, boolean reviewed) {
+    public User(Long id, String mail, String name, String password, /*String password2,*/ boolean reviewed) {
         this.id = id;
         this.mail = mail;
         this.name = name;
         this.password = password;
         this.reviewed = reviewed;
-        this.password2 = password2;
+        // this.password2 = password2;
     }
 
     public Long getId() {
@@ -34,9 +34,9 @@ public class User {
         return password;
     }
 
-    public String getPassword2() {
-        return password2;
-    }
+    // public String getPassword2() {
+    //     return password2;
+    // }
 
     public boolean getReviewed() {
         return reviewed;
